6e57bfb30afd4474640c7a4327d36f632708d163
Nikolaipe <nikolaipe@getacademy.no>
2023-11-10 11:12:57 +0100

    Many rewrites
    Been trying different things. Break time now.

--- GameBoard.gd
+++ GameBoard.gd
@@ -1,13 +1,5 @@
 extends CanvasLayer
 
-var current_orb = []
-var next_orb = []
-var all_orbs = []
-var stop_left = false
-var stop_right = false
-
-var speed = 10
-
 @export var red_orb_scene: PackedScene
 @export var orange_orb_scene: PackedScene
 @export var yellow_orb_scene: PackedScene
@@ -16,52 +8,53 @@ var speed = 10
 @export var violet_orb_scene: PackedScene
 @export var pride_orb_scene: PackedScene
 
+var current_orb
+var next_orb
+var speed = 10
+var all_orbs = []
+var stop_left = false
+var stop_right = false
+
 func _ready():
 	var start_orb = red_orb_scene.instantiate()
+	start_orb.position = $PlacementMarker.position
 	start_orb.gravity_scale = 0
-	start_orb.position = $NextMarker.position
 	add_child(start_orb)
-	next_orb.push_front(start_orb)
-	new_orb()
+	current_orb = start_orb
+	next_orb = random_next_orb()
+
+
+func random_next_orb():
+	var random_num = randi_range(1, 3)
+	var temp_next
+	if random_num == 1:
+		temp_next = red_orb_scene.instantiate()
+	elif random_num == 2:
+		temp_next = orange_orb_scene.instantiate()
+	elif random_num == 3:
+		temp_next = yellow_orb_scene.instantiate()
+	temp_next.position = $NextMarker.position
+	temp_next.gravity_scale = 0
+	add_child(temp_next)
+	return temp_next
+
 
 func get_input():
-	var input_direction
-	if !stop_left and Input.is_action_just_pressed("move_left"):
-		input_direction = Vector2(-1, 0)
-	elif !stop_right and Input.is_action_just_pressed("move_right"):
-		input_direction = Vector2(1, 0)
-	else:
-		input_direction = Vector2.ZERO
-	$PlacementMarker.position = $PlacementMarker.position + input_direction * speed
+	#TODO: use _physics_prosess and move_and_slide on orbs
+	pass
 
-func _process(delta):
-	current_orb[0].position = $PlacementMarker.position
-	get_input()
-	if Input.is_action_just_pressed("drop"):
-		drop()
-	
 
-func new_orb():
-	$PlacementMarker.position = Vector2(512, 56)
-	current_orb.push_front(next_orb[0])
-	next_orb.clear()
-	next_orb.push_front(get_random_orb())
-	
-	
+func _on_stop_left_body_entered(_body):
+	stop_left = true
 
-func get_random_orb():
-	var rand_num = randi_range(1, 3)
-	var rand_orb
-	if rand_num == 1 :
-		rand_orb = red_orb_scene.instantiate()
-	elif rand_num == 2 :
-		rand_orb = orange_orb_scene.instantiate()
-	elif rand_num == 3 :
-		rand_orb = yellow_orb_scene.instantiate()
-	rand_orb.position = $NextMarker.position
-	rand_orb.gravity_scale = 0
-	add_child(rand_orb)
-	return rand_orb
 
-func drop():
-	#TODO add a timer and shit to make this work
+func _on_stop_left_body_exited(_body):
+	stop_left = false
+
+
+func _on_stop_right_body_entered(_body):
+	stop_right = true
+
+
+func _on_stop_right_body_exited(_body):
+	stop_right = false
